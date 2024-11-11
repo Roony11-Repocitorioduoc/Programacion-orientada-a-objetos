943d374072fba6202c112593ce90c78450c98067
Roony11-Repocitorioduoc <166177021+Roony11-Repocitorioduoc@users.noreply.github.com>
2024-11-11 16:53:59 -0300

    Delete FireEmblemConsola directory

--- FireEmblemConsola/src/fireemblemconsola/Hacha.java
+++
@@ -1,9 +0,0 @@
-package fireemblemconsola;
-
-public class Hacha extends Arma
-{ 
-    public Hacha(int daño, int acierto, int critico, int peso, String nombre)
-    {
-        super(daño, acierto, critico, peso, nombre, "Arma pesada,\npierde ante las espadas,\ngana contra las lanzas");
-    }
-}
--- FireEmblemConsola/src/fireemblemconsola/Lanza.java
+++
@@ -1,9 +0,0 @@
-package fireemblemconsola;
-
-public class Lanza extends Arma
-{ 
-    public Lanza(int daño, int acierto, int critico, int peso, String nombre)
-    {
-        super(daño, acierto, critico, peso, nombre, "Arma balanceada,\npierde ante las hachas,\ngana contra las espadas");
-    }
-}
